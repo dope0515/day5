5c08789a275d8ccd6fc27ae5f4bd89add4d65424
dope0515 <abpo4243@gmail.com>
2023-07-09 13:44:44 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,26 @@
-//문자열로 반복되는 태그를 만들어서 innerHTML로 동적으로 pic0.jpg~pic199.jpg까지의 img태그 200개를 동적으로 figure영역 안쪽에 생성
-
-//동적으로 200개의 태그가 문자열로 담길 빈 문자열을 생성한다음 반복문으로 200번 반복돌면서 태그 문자열생성하고 innerHTML로 figure안쪽에 생성
-const frame = document.querySelector('figure');
 const num = 200;
 
-let tags = '';
-//for (let i = 0; i < num; i++) tags += `<img src='img/pic${i}.jpg' />`;
-Array(num)
-	.fill()
-	.forEach((_, idx) => (tags += `<img src='img/pic${idx}.jpg' />`));
+//반환된 이미지를 전역변수에 담음
+const imgDOM = createImgs('figure', num);
 
-frame.innerHTML = tags;
+window.addEventListener('mousemove', (e) => matchMove(imgDOM, num, e));
 
-const imgDOM = frame.querySelectorAll('img');
+//동적으로 이미지 생성후 반환 함수
+function createImgs(targetEl, num) {
+	const frame = document.querySelector(targetEl);
+	let tags = '';
+	Array(num)
+		.fill()
+		.forEach((_, idx) => (tags += `<img src='img/pic${idx}.jpg' />`));
+	frame.innerHTML = tags;
+	return frame.querySelectorAll('img');
+}
 
-window.addEventListener('mousemove', e => {
+//마우스 포인터 위치에 따라 이미지 순서매칭
+function matchMove(arrEl,num,e){
     const percent = parseInt((e.clientX / window.innerWidth) * num);
-    console.log(percent);
+	console.log(percent);
 
-    for(const img of imgDOM) img.style.display = 'none';
-    imgDOM[percent].style.display="block"
-});
+	for (const img of arrEl) img.style.visibility = 'hidden';
+	arrEl[percent].style.visibility = 'visible';
+}
